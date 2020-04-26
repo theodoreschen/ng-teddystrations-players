e3dfe1e503d5e1651bdfc3736987ae5bd6244b63
theodoreschen <theodoreschen@gmail.com>
2020-04-25 23:53:01 -0400

    converting to teddystrations

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-telestrations-players'),
+      dir: require('path').join(__dirname, './coverage/ng-teddystrations-players'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-telestrations-players'`, () => {
+  it(`should have as title 'ng-teddystrations-players'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-telestrations-players');
+    expect(app.title).toEqual('ng-teddystrations-players');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-telestrations-players app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-teddystrations-players app is running!');
   });
 });
